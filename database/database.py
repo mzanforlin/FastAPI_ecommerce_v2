de70170cc00e036b02cdd987dda457b44d54fbc6
Matheus Zanforlin <zanforlin14@gmail.com>
2025-05-03 16:27:07 -0300

    ultimo

--- database/database.py
+++ database/database.py
@@ -1,8 +1,8 @@
-from database import engine  # Aqui você importa corretamente o engine
+from database.database import engine  # Aqui você importa corretamente o engine
 from models import base
 from models.tb_usuarios import Usuarios
 
 # Criar todas as tabelas
-Base.metadata.create_all(bind=engine)
+base.metadata.create_all(bind=engine)
 
 print("Tabelas criadas com sucesso!")
